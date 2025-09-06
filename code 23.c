2849466ec99345db9136d0be346b3317be1b115d
EKTA BHARDWAJ <ebhardwaj743@gmail.com>
2025-09-06 18:43:17 +0530

    Update code 23.c

--- code 23.c
+++ code 23.c
@@ -1,8 +1,8 @@
-Q23: Write a program to calculate library fine based on late days as follows: 
+/*Q23: Write a program to calculate library fine based on late days as follows: 
 First 5 days late: ₹2/day 
 Next 5 days late: ₹4/day 
 Next 20 days days late: ₹6/day 
-More than 30 days: Membership Cancelled.
+More than 30 days: Membership Cancelled.*/
 
 #include <stdio.h>
 
@@ -34,3 +34,4 @@ int main() {
 
     return 0;
 }
+
