d3773f63c3b25bcb3dfb1b3ef7f061a3846db4a5
EKTA BHARDWAJ <ebhardwaj743@gmail.com>
2025-09-06 18:45:35 +0530

    Update code 3.c

--- code 3.c
+++ code 3.c
@@ -1,4 +1,4 @@
-Q3: Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+//Q3: Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
 
 #include <stdio.h>
 
@@ -18,3 +18,4 @@ int main() {
 
     return 0;
 }
+
